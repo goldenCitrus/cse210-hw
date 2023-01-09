1cd287324da80c0a5c0086898024b117326c6314
Matthew Sorensen <matthew.sorensen03@gmail.com>
2023-01-09 13:03:03 -0700

    completed C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        float grade = float.Parse(Console.ReadLine());
+        Console.WriteLine(grade);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You have a B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You have a C");
+        }
+        else
+        {
+            Console.WriteLine("You are failing");
+        }
     }
 }
