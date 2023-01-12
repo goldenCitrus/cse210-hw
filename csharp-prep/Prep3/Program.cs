9d3db2d2bb8b0137c06347996401ddc2a8b86e0e
Matthew Sorensen <matthew.sorensen03@gmail.com>
2023-01-12 12:55:16 -0700

    completed Prep#3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Chose a magic number (above 0): ");
+        int mn = Int16.Parse(Console.ReadLine());
+        int guess = 0;
+
+        do
+        {
+            Console.WriteLine("What is your guess: ");
+            guess = Int16.Parse(Console.ReadLine());
+
+            if (guess < mn)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (guess != mn);
+
+        Console.WriteLine("You win lol");
     }
 }
